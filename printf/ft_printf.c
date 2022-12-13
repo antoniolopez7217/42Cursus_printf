c0db130f89fa6157c9d601b44fede6e52258e30f
Antonio Lopez Chamorro <antlopez@c1r13s2.42malaga.com>
2022-12-13 18:06:42 +0100

    updated

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -12,20 +12,20 @@
 
 #include "libftprintf.h"
 
-int	ft_printf(char const *f, ...)
+int	ft_printf(char const *str, ...)
 {
 	t_list	args_lst;
 	va_list	args;
 	int		i;
 
-	va_start(args, f);
+	va_start(args, str);
 	i = 0;
-	while (i < f)
+	while (i < str)
 	{
-		ft_lstadd_back(args_lst, ft_lstnew(va_arg(args, char const)));
+		ft_lstadd_back(args_lst, ft_lstnew(va_arg(args, char const))); //Crea un nodo y se le añade al final de la lista para ir incluyendo los args
 		i++;
 	}
-	if (ft_check_str(args_lst) == 0)
+	if (ft_check_str(args_lst) == 0) // Recorre la str y devuelve 0 si hay error
 		return (0);
 	return (1);
 }
@@ -39,13 +39,13 @@ int	ft_check_str(t_list *lst)
 	i = 0;
 	while(lst[0][i].content)
 	{
-		if(i == 0 || lst[0][i+1].content == '\0')
+		if(i == 0 || lst[0][i+1].content == '\0') //Comprueba que el primero y el ultimo char sea '"'
 			if(lst[0][i].content != '"')
 				return (0);
 			else
 				i++;
 				continue;
-		if (lst[0][i].content != '%')
+		if (lst[0][i].content != '%') //Comprueba el formato tras %
 		{
 			ft_putchar_fd(lst[0][i].content, 1);
 			i++;
